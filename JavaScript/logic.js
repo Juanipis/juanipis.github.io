55533c2eda34b2ec1f12562d0a80d76ffc97de89
Juan Pablo Díaz Correa <juanipis@gmail.com>
2020-12-04 23:19:40 -0500

    Arreglo nuevo para el botón

--- JavaScript/logic.js
+++ JavaScript/logic.js
@@ -1,4 +1,6 @@
 var colorWeb = true;
+var stylesheet_css = document.styleSheets[1];
+stylesheet_css.insertRule("#daynightButtom:hover{color: #1FB58F;background: #2b2e2c;border: 2px solid #1FB58F;border-radius: 6px;}");
 
 function daynight(){
     if(colorWeb){
@@ -23,6 +25,10 @@ function setColorsWeb(background, colorTitle , colorText,  lineTitle, footerColo
     var footerCopyright = document.getElementsByClassName("copyright");
     var footerFlaticon = document.getElementsByClassName("flaticon");
     var buttomState = document.getElementById("daynightButtom");
+    
+
+    //IMPORTANT
+    //stylesheet_css.insertRule("#page-indice{display: flex;justify-content: center;margin-top: 3.5rem;margin-bottom: 0.2rem;margin-right: 30%;margin-left: 30%;padding-top: 6%;font-size: 250%;font-family: Arial, Helvetica, sans-serif; text-align: center;border-bottom-style: solid;border-color: #EAB126;}");
 
     //Header
     header.style.background = background;
@@ -61,12 +67,16 @@ function setColorsWeb(background, colorTitle , colorText,  lineTitle, footerColo
         buttomState.innerText = "Modo día 🌞";
         buttomState.style.color = "#EAB126";
         buttomState.style.borderColor = "#EAB126";
+        stylesheet_css.deleteRule("#daynightButtom:hover { color: rgb(234, 177, 38); background: rgb(234, 177, 38) none repeat scroll 0% 0%; border: 2px solid rgb(31, 181, 143); border-radius: 6px; }");
+        stylesheet_css.insertRule("#daynightButtom:hover{color: #1FB58F;background: #2b2e2c;border: 2px solid #1FB58F;border-radius: 6px;}");
     }
     else
     {
         buttomState.innerText = "Modo noche 🌙";
         buttomState.style.color = "#1FB58F";
         buttomState.style.borderColor = "#1FB58F";
+        stylesheet_css.deleteRule("#daynightButtom:hover{color: #1FB58F;background: #2b2e2c;border: 2px solid #1FB58F;border-radius: 6px;}");
+        stylesheet_css.insertRule("#daynightButtom:hover { color: rgb(234, 177, 38); background: rgb(234, 177, 38) none repeat scroll 0% 0%; border: 2px solid rgb(31, 181, 143); border-radius: 6px; }")
     }
     
 }
